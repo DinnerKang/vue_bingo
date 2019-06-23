eb3fe75997145f4c7109f76e270d4dae7a0d8f51
DinnerKang <jenhyuk0318@gmail.com>
2019-06-23 21:21:50 +0900

    Create bingo container
    빙고 틀 만들기 + 클릭시 색상

--- README.md
+++ README.md
@@ -1,29 +1,6 @@
 # vue_bingo
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### 계획
+1. 빙고 컴포넌트 만들기
+2. 빙고 체크
+3. 5 빙고면 게임 끝
---
+++ src/components/bingo/Bingo.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="bingo_container">
+            <div class="bingo" v-for="list in 25" v-bind:key="list" v-on:click="clickBingo(list)">
+                {{list}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Bingo',
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        clickBingo: function(number){
+            if(document.getElementsByClassName("bingo")[number-1].style.backgroundColor === "yellow"){
+                document.getElementsByClassName("bingo")[number-1].style.backgroundColor = "#fff";
+            }else{
+                document.getElementsByClassName("bingo")[number-1].style.backgroundColor = "yellow";
+            }
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.bingo_container{
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+
+    .bingo{
+        flex: 0 0 19%;
+        border: 1px solid #ced3d6;
+        height: 150px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 40px;
+        cursor: pointer;
+    }
+
+    .active_bingo{
+        background-color: yellow;
+    }
+}
+
+
+</style>
+
