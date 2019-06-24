bcaecf9b01c730743082087a76e2c24ed2d898bf
DinnerKang <jenhyuk0318@gmail.com>
2019-06-24 20:45:22 +0900

    Shack bingo function
    빙고 섞기 기능 완성

--- src/components/bingo/Bingo.vue
+++ src/components/bingo/Bingo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="bingo_container">
-            <div class="bingo" v-for="list in 25" v-bind:key="list" v-on:click="clickBingo(list)">
+            <div class="bingo" v-for="list in this.bingoBoard" v-bind:key="list" v-on:click="clickBingo(list)">
                 {{list}}
             </div>
         </div>
@@ -13,16 +13,46 @@ export default {
     name: 'Bingo',
     data(){
         return{
-            
+            bingoBoard: [],
+            bingo: [],
+            verticalBingo : 0,
+            horizontalBingo : 0,
+            diagonalBingo: 0
         }
     },
+    created(){
+        this.shackBingo();
+    },
     methods: {
-        clickBingo: function(number){
-            if(document.getElementsByClassName("bingo")[number-1].style.backgroundColor === "yellow"){
-                document.getElementsByClassName("bingo")[number-1].style.backgroundColor = "#fff";
-            }else{
-                document.getElementsByClassName("bingo")[number-1].style.backgroundColor = "yellow";
+        shackBingo: function(){
+            
+            var arr = new Array(25);
+            var temp = [];
+
+            for(var i=1;i<=25;i++){
+                arr[i] = i;
             }
+
+            arr.sort(function(){return 0.5-Math.random()});
+            this.bingoBoard = arr;
+
+            for(let j=0, i=0; i<25;i++){
+                temp[i%5] = arr[i];
+
+                if((i+1) % 5 === 0){
+                    this.bingo[j] = temp;
+                    j++;
+                    temp = [];
+                }
+            }
+            console.log(this.bingo);
+        },
+
+        clickBingo: function(number){
+            document.getElementsByClassName("bingo")[number-1].style.backgroundColor = "yellow";
+        },
+        checkVertical: function(){
+            this.verticalBingo++;
         }
     },
 }
@@ -34,7 +64,6 @@ export default {
     display: flex;
     width: 100%;
     flex-wrap: wrap;
-
     .bingo{
         flex: 0 0 19%;
         border: 1px solid #ced3d6;
