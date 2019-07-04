dd38b24a0462ed88dc788acd9e4a029d1a7c0578
DinnerKang <jenhyuk0318@gmail.com>
2019-07-04 17:17:21 +0900

    Test reset btn

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <article class="btn_container center">
+      <input type="button" class="btn" name="resetBtn" id="resetBtn" value="게임 초기화">
+    </article>
     <article class="main_container">
       <Bingo user="user_1"></Bingo>
     </article>
     <article class="main_container">
       <Bingo user="user_2"></Bingo>
     </article>
-    <article v-if="winner">
+    <article v-if="winner" class="center">
       승자 : {{winner}}
     </article>
   </div>
@@ -32,5 +35,11 @@ export default {
   .main_container{
     width: 50%;
     float: left;
+  }
+  .btn_container{
+    margin: 50px 0;
+  }
+  .center{
+    text-align: center;
   }
 </style>
